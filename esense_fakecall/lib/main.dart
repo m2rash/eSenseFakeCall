9ba2b29145b8631da0aa42157aa7524991e41a0c
Lukas <Lukas.schoelch@gmail.com>
2020-04-15 15:09:54 +0200

    New Icon and Name

--- esense_fakecall/lib/main.dart
+++ esense_fakecall/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'eSense MC',
+      title: 'Help!',
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
